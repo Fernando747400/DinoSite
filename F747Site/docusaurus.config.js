cc2edeabfcbebec0e0f893636da106a87c7971dd
Fernando747 <fernandojcr5@gmail.com>
2023-03-08 04:26:02 -0600

    Site config changed

--- F747Site/docusaurus.config.js
+++ F747Site/docusaurus.config.js
@@ -11,16 +11,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://fernando747400.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/F747Site/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Fernando747400', // Usually your GitHub org/user name.
   projectName: 'F747Site', // Usually your repo name.
   deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
